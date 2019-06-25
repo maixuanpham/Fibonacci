9b8c0687564e09580f7212c0e29dbb9e2014c17b
maixuanpham <51069707+maixuanpham@users.noreply.github.com>
2019-06-25 16:49:38 -0700

    Update fibonacci.asm

--- fibonacci.asm
+++ fibonacci.asm
@@ -11,7 +11,7 @@ prompt3 BYTE	"Sum: ", 0
 prompt4 BYTE	"Last Value: ", 0
 prompt5	BYTE	"Extra credit using array and procedure:", 0
 
-sum		DWORD	?
+sum	DWORD	?
 lValue	DWORD	?
 myArray	DWORD	25 DUP(0), 0
 
@@ -27,15 +27,15 @@ main PROC
 
 	mov edx, OFFSET prompt1			; ask for input value
 	call WriteString
-	call ReadInt					; read integer into EAX
-	push eax						; save for extra credit
+	call ReadInt				; read integer into EAX
+	push eax				; save for extra credit
 
 	mov edx, OFFSET prompt2			; display fibonacci sequence
 	call WriteString
 	
-	call fibNumbers					; call procedure				
-	mov sum, edx					; save sum
-	mov lValue, eax					; save last value
+	call fibNumbers				; call procedure				
+	mov sum, edx				; save sum
+	mov lValue, eax				; save last value
 	call crlf
 
 	mov edx, OFFSET prompt3			; output sum		
@@ -57,27 +57,27 @@ main PROC
 	call crlf
 
 	mov esi, OFFSET myArray			; get array address
-	pop ecx							; get n times from stack
+	pop ecx					; get n times from stack
 	call fibNumbers2
 
 	mov edx, OFFSET prompt2			; display fibonacci sequence
 	call WriteString
 
-	inc ecx							; print sequence
+	inc ecx					; print sequence
 	call printArray
 	call crlf
 
 	mov edx, OFFSET prompt3			; output sum		
 	call WriteString
-	call arraySum					; get sum
-	mov sum, eax					; save sum
+	call arraySum				; get sum
+	mov sum, eax				; save sum
 	call WriteDec
 	call crlf
 
 	mov edx, OFFSET prompt4			; output last value
 	call WriteString
-	call getLastValue				; get last value
-	mov lValue, eax					; save last value
+	call getLastValue			; get last value
+	mov lValue, eax				; save last value
 	call WriteDec
 	call crlf
 	call crlf
@@ -86,79 +86,79 @@ main PROC
 main ENDP
 
 fibNumbers PROC
-	mov ecx, eax					; loop amount of n times
-	mov edx, 0						; sum start at 0
+	mov ecx, eax				; loop amount of n times
+	mov edx, 0				; sum start at 0
 	mov eax, 0
 	mov ebx, 1
-	call WriteDec					; display sequence
+	call WriteDec				; display sequence
 L1:
-	add eax, ebx					; add last two numbers
-	add edx, ebx					; add to sum
-	xchg eax, ebx					; exchange the two last number
-	push eax						; save eax
-	mov al, ' '						; display space
+	add eax, ebx				; add last two numbers
+	add edx, ebx				; add to sum
+	xchg eax, ebx				; exchange the two last number
+	push eax				; save eax
+	mov al, ' '				; display space
 	call WriteChar
 	pop eax
-	call WriteDec					; display sequence
+	call WriteDec				; display sequence
 	loop L1
 	ret
 fibNumbers ENDP
 
 fibNumbers2 PROC
-	push esi						; save address and n times
+	push esi				; save address and n times
 	push ecx
 	mov eax, 0
 	mov ebx, 1
-	mov [esi], eax					; store into array
+	mov [esi], eax				; store into array
 L1:
-	add eax, ebx					; add last two numbers
-	xchg eax, ebx					; exchange the two last number
+	add eax, ebx				; add last two numbers
+	xchg eax, ebx				; exchange the two last number
 	add esi, TYPE myArray			; move to next index
-	mov [esi], eax					; store into array
+	mov [esi], eax				; store into array
 	loop L1
 
-	pop ecx							; restore address and n times
+	pop ecx					; restore address and n times
 	pop esi
 	ret
 fibNumbers2 ENDP
 
 arraySum PROC
-	push esi						; save address and n times
+	push esi				; save address and n times
 	push ecx
-	mov eax, 0						; set to 0
+	mov eax, 0				; set to 0
 L1: 
-	add eax, [esi]					; add sum as it loop through index
+	add eax, [esi]				; add sum as it loop through index
 	add esi, TYPE myARRAY
 	loop L1
 
-	pop ecx							; restore address and n times
+	pop ecx					; restore address and n times
 	pop esi
 	ret
 arraySum ENDP
 
 printArray PROC
-	push esi						; save address and n times
+	push esi				; save address and n times
 	push ecx
 L1: 
-	mov eax, [esi]					; print value
+	mov eax, [esi]				; print value
 	call WriteDec	
-	mov al, ' '						; print space
+	mov al, ' '				; print space
 	call WriteChar
 	add esi, TYPE myArray
 	loop L1
 
-	pop ecx							; restore address and n times
+	pop ecx					; restore address and n times
 	pop esi
 	ret
 printArray ENDP
 
 getLastValue PROC
-	mov eax, ecx					; get the number of value in an array
-	dec eax							; dec to go to the last value
+	mov eax, ecx				; get the number of value in an array
+	dec eax					; dec to go to the last value
 	mov ebx, TYPE myArray			; mul by the type of array to get position
 	mul ebx
 	add esi, eax
-	mov eax, [esi]					; move that value into eax
+	mov eax, [esi]				; move that value into eax
 	ret
 getLastValue ENDP
 
